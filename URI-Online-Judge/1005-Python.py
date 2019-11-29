b7da6ddeda20fbbb52bcb1e1d79444819294a83b
yurinogueira <yurinogueira@id.uff.br>
2019-11-29 17:49:04 -0300

    Alguns outros problemas resolvidos

---
+++ URI-Online-Judge/1005-Python.py
@@ -0,0 +1,6 @@
+A = float(input())
+B = float(input())
+A1 = A * 3.5
+B1 = B * 7.5
+S = (A1 + B1) / 11
+print("MEDIA = %.5f" % S)
---
+++ URI-Online-Judge/1006-Python.py
@@ -0,0 +1,8 @@
+A = float(input())
+B = float(input())
+C = float(input())
+A *= 2
+B *= 3
+C *= 5
+S = (A + B + C) / 10
+print("MEDIA = %.1f" % S)
