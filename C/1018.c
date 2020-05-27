70fee60b9cda16df19e2fd1829f9caaf4a6eb0fb
yurinogueira <yurinogueira@id.uff.br>
2020-05-26 22:50:09 -0300

    C

--- C/1018.c
+++ C/1018.c
@@ -7,13 +7,10 @@ int main()
     int n_100 = a / 100;
     printf("%d nota(s) de R$ 100,00\n", n_100);
     int n_50 = (n_100 * 100) - 50;
-    if (n_50 >= 50)
-    {
+    if (n_50 >= 50) {
         n_50 = n_50 - 50;
         printf("%d nota(s) de R$ 100,00\n", 1);
-    }
-    else
-    {
+    } else {
         printf("0 nota(s) de R$ 50,00\n");
     }
     int n_20 = n_50 - 20;
